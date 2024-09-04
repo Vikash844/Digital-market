44ad068c6ea6295e16d0d3b68262e88980efb33a
Vikash844 <vikashsingh844855@gmail.com>
2024-09-04 22:46:56 +0530

    small changes

--- src/component/pages/home/OurSevices.jsx
+++ src/component/pages/home/OurSevices.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const OurSevices = () => {
- const items = [
-    {
-        id: 1,
-        content: "Social Media Marketing",
-        para: "Build a Strong Social Presence"
-    },
-    {
-        id: 2,
-        content: "Search Engine Optimization",
-        para: "Boost Your Online Visibility",
-    },
-    {
-        id: 3,
-        content: "Content Creation",
-        para: "Engage and Inspire Your Audience"
-    },
-    {
-        id: 1,
-        content: "Web Development",
-        para: "Build Your Digital Home and boost website"
-    }
- ]
+    const items = [
+        {
+            id: 1,
+            content: "Social Media Marketing",
+            para: "Build a Strong Social Presence"
+        },
+        {
+            id: 2,
+            content: "Search Engine Optimization",
+            para: "Boost Your Online Visibility",
+        },
+        {
+            id: 3,
+            content: "Content Creation",
+            para: "Engage and Inspire Your Audience"
+        },
+        {
+            id: 1,
+            content: "Web Development",
+            para: "Build Your Digital Home and boost website"
+        }
+    ]
     return (
         <div className="our-portfolio section">
             <div className="container">
@@ -35,18 +35,22 @@ const OurSevices = () => {
                     </div>
                 </div>
                 <div className="row g-3">
-                    {
-                        items.map((item, index) => (
-                            <div className="col-lg-3 col-sm-6" key={index}>
+                    {items.map((item, index) => (
+                        <div className="col-lg-3 col-sm-6" key={index}>
                             <div className="showed-content">
                                 <h4>{item.content}</h4>
                                 <p>{item.para}</p>
-                                <Link className='btn btn-info text-light mt-2' to="/services">read more</Link>
+                                <Link className="btn btn-info text-light mt-2" to="/services">
+                                    Read More
+                                </Link>
                             </div>
-                        </div> 
-                        ))
-                    }
+                        </div>
+                    ))}
                 </div>
+                <div className="text-center my-3">
+                    <Link to="/services" className="btn btn-info text-light mt-2">More Services</Link>
+                </div>
+
             </div>
         </div>
     )
