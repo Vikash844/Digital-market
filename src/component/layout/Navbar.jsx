f5313a02ceb820b50d19e1a1e87de8c82e02c2f5
Vikash844 <vikashsingh844855@gmail.com>
2024-09-15 22:49:31 +0530

    some changes

--- src/component/layout/Navbar.jsx
+++ src/component/layout/Navbar.jsx
@@ -2,52 +2,58 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <header className="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
-            <div className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container justify-content-around">
-                    {/* Logo */}
-                    <div className="navbar-brand">
-                        <Link className="logo" to="/">
-                            <img src="/assets/images/logo.png" alt="Company Logo" className="w-200 h-75 img-fluid" />
-                        </Link>
-                    </div>
+  return (
+    <header className="header-area wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container justify-content-around">
+          {/* Logo */}
+          <div className="navbar-brand">
+            <Link className="logo" to="/">
+              <img src="/assets/images/logo.png" alt="logo" className="w-200 h-75 img-fluid" />
+            </Link>
+          </div>
 
-                    {/* Hamburger Menu Button */}
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+          {/* Hamburger Menu Button */}
+          <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-                    {/* Navbar Links */}
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item me-2">
-                                <Link className="nav-link" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item me-2">
-                                <Link className="nav-link" to="/about-us">About Us</Link>
-                            </li>
-
-                            {/* Dropdown for Services */}
-                            <li className="nav-item dropdown me-2">
-                                <Link className="nav-link dropdown-toggle" to="#" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Services
-                                </Link>
-                                <ul className="dropdown-menu" aria-labelledby="servicesDropdown">
-                                    <li><Link className="dropdown-item" to="/">SEO Marketing</Link></li>
-                                    {/* Add more items here if needed */}
-                                </ul>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact-us">Contact Now</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
+          {/* Navbar Links */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item mx-2">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item mx-2">
+                <Link className="nav-link" to="/about-us">About Us</Link>
+              </li>
+              <li className="nav-item dropdown mx-2">
+                <Link className="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Services</Link>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item text-black" to="/">SEO Marketing</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Content Writing</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Social Media Marketing</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Content Creation</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Web Development</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Logo Creation</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Influencer Marketing</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Analytics & Reporting</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Email Marketing</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">PPC Marketing</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Shopify Store Creation & Management</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Conversion Rate Optimization (CRO)</Link></li>
+                  <li><Link className="dropdown-item text-black" to="/">Online Reputation Management (ORM)</Link></li>
+                </ul>
+              </li>
+              <li className="nav-item mx-2">
+                <Link className="nav-link" to="/contact-us">Contact Now</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
